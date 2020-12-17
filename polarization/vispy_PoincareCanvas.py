52314e09e8182b9623aa9c7f6569cfc60b98b07f
dvsphanindra <dvsphanindra@gmail.com>
2020-12-17 17:24:14 +0530

    Minor changes to Polarization code. Added images to 'gallery' directory

--- polarization/vispy_PoincareCanvas.py
+++ polarization/vispy_PoincareCanvas.py
@@ -58,22 +58,27 @@ class SoP:
 	def __init__(self, stokes_vector, parent, label="P0", color='red'):
 		self.stokes_vector=stokes_vector
 		self.intensity=self.stokes_vector[0]
-		self.polarizationVector= self.stokes_vector[1:] / self.stokes_vector[0]
+		self.polarizationVector= self.stokes_vector[1:] #/ self.stokes_vector[0] # TODO: To be verified
 		self.label=label
 		self.color=color
+		self.validate_StokesVector()
 		# •◊×
 		scene.Text("•", font_size=100, bold=True, color=color, parent=parent, pos=self.polarizationVector)
 		self.labelText=scene.Text(self.label, font_size=50, bold=True, color=self.color, parent=parent, pos=self.polarizationVector + (0.15 * self.polarizationVector))
 		
 	def get_PolarizationVector(self):
 		return self.polarizationVector
-	
 	def get_label(self):
 		return self.label
 	def get_color(self):
 		return self.color
 	def get_intensity(self):
 		return self.intensity
+	def get_DegreeOfPolarization(self):
+		return np.sqrt(np.sum(np.square(self.polarizationVector)))/self.stokes_vector[0]
+	def validate_StokesVector(self):
+		#
+		assert np.sqrt(np.sum(np.square(self.polarizationVector)))<=self.stokes_vector[0], "Irregular Stokes Vector: S0, vector, square, sum of square: {0}, {1}, {2}, {3}".format(self.stokes_vector[0], self.polarizationVector, np.square(self.polarizationVector), np.sum(np.square(self.polarizationVector)))
 	# def get_ellipticity(self):
 	# 	return np.tan(np.pi/4-self.Xi)
 	# def get_orientation(self):
@@ -136,7 +141,7 @@ class Retarder:
 		w = np.array((0,0,0)) # Line start point
 		si = np.dot(self.retarderDirection, incoming_SoP.get_PolarizationVector()) / dotProduct
 		center = w + si * self.retarderDirection
-		# print(self.rotationQuaternion, si, center)
+		print("center=",self.retarderDirection, si, center)
 		# scene.Text("◊", font_size=70, bold=True, color=self.color, parent=self.parent, pos=center) # To display the center point
 		
 		# Draw the arc of rotation of the quaternion
@@ -145,22 +150,23 @@ class Retarder:
 		angle_of_circle=np.arccos(np.dot(self.retarderDirection, incoming_SoP.get_PolarizationVector()))
 		
 		# Find the starting direction of circle to determine where to start the arc in the visual
-		q = Quaternion(axis=(0, 0, 1), radians=angle_of_circle)
+		q = Quaternion(axis=(0, 0, 1), radians=angle_of_circle) # Axis is z-axis (S3 in the visual)
 		start_of_circle=q.rotate(np.array(self.retarderDirection))
 		start_vector=start_of_circle-center
 		SoP_vector=incoming_SoP.get_PolarizationVector()-center
 		angle_between_vectors=np.arccos(np.dot(start_vector/np.linalg.norm(start_vector), SoP_vector/np.linalg.norm(SoP_vector)))
-		# print(start_vector, SoP_vector, np.rad2deg(angle_between_vectors))
+		angle_between_vectors = np.sign(SoP_vector[2]) * angle_between_vectors # Multiply with the sign of the S3 coordinate to determine if the rotation is in the lower hemisphere
+		print("Angle=", start_vector, SoP_vector, np.rad2deg(angle_between_vectors), np.rad2deg(angle_of_circle))
 		
 		# Draw the arc using the above data
 		t = np.linspace(angle_between_vectors, angle_between_vectors+np.deg2rad(2 * self.delta), 100)
 		y = r * np.cos(t)
 		z = r * np.sin(t)
 		x = np.zeros(y.size)
-		line = LinePlot((x, y, z), width=15, color=self.color, parent=self.parent)
-		line.transform = scene.transforms.MatrixTransform()
-		line.transform.rotate(2 * self.theta, (0, 0, 1))
-		line.transform.translate(center)
+		arc = LinePlot((x, y, z), width=15, color=self.color, parent=self.parent)
+		arc.transform = scene.transforms.MatrixTransform()
+		arc.transform.rotate(2 * self.theta, (0, 0, 1))
+		arc.transform.translate(center)
 		
 		# Draw arrow head at the center (51st point) of the above arc to indicate the direction of rotation
 		arrowHead = np.array([(x[50],y[50],z[50],x[51],y[51],z[51])])  # Arrow direction, position -Direction determined by the 50th point
@@ -216,9 +222,27 @@ class Polariser:
 		:param incoming_SoP: incoming state of polarization
 		:return: resulting state of polarization after polarisation
 		"""
+		# gamma is the angle between the orientation of the polariser and orientation of the incoming SoP
 		gamma=np.arccos(np.dot(self.polariserDirection, incoming_SoP.get_PolarizationVector()))
-		print("gamma=",np.rad2deg(gamma))
-		result_SoP=np.array((np.cos(gamma/2)**2, np.cos(self.theta), np.sin(self.theta),0))
+		two_psi=np.arctan(self.polariserDirection[1]/self.polariserDirection[0])
+		if np.abs(self.theta)>45:
+			two_psi+=np.pi
+		I=np.cos(gamma / 2) ** 2
+		# From the basic relations for S0, S1, S2, S3 given in https://en.wikipedia.org/wiki/Stokes_parameters
+		# DoP=1, two_Xi=0, the relations simplify
+		result_polarization_vector=np.array((I*np.cos(two_psi), I*np.sin(two_psi), 0))
+		result_SoP = SoP(np.append([I],result_polarization_vector), parent=self.parent, color=incoming_SoP.get_color(), label=incoming_SoP.get_label() + 'x' + self.label)
+		# print("In Polariser: gamma, 2Psi, result=",np.rad2deg([gamma, two_psi]), result_polarization_vector)
+		
+		# Draw the transformation visual
+		# Calculate mid point for drawing the arrow head
+		mid_point=(incoming_SoP.get_PolarizationVector()+result_polarization_vector)/2
+		arrowHead = np.array([np.append(incoming_SoP.get_PolarizationVector(), mid_point)]) # Arrow direction, position
+		print("arrow=",arrowHead)
+		Arrow(pos=np.array([incoming_SoP.get_PolarizationVector(), result_polarization_vector]), color=self.color, method='gl', width=5.,
+		                             arrows=arrowHead,
+		                             arrow_type="angle_30", arrow_size=5.0, arrow_color=self.color, antialias=True,
+		                             parent=self.parent)
 		return result_SoP
 
 
@@ -227,14 +251,14 @@ view = canvas.central_widget.add_view()
 view.camera = scene.TurntableCamera(up='z', fov=30)
 
 canvas.bgcolor = Color(color="lightsteelblue", alpha=0.5)
-retarder1 = Retarder(130, 45, parent=view.scene, color='teal', label='R0')
+retarder1 = Retarder(30, 35, parent=view.scene, color='teal', label='R0')
 
-sop1=SoP(np.array((1,0,0,1)), parent=view.scene, color='white', label="SoP1")
+sop1=SoP(np.array((1,0,1,0)), parent=view.scene, color='white', label="SoP1")
 sop2=retarder1.retard(sop1)
 
-polariser1=Polariser(0, parent=view.scene, color='yellow', label="P1")
+polariser1=Polariser(20, parent=view.scene, color='yellow', label="P1")
 out_SoP=polariser1.polarise(sop2)
-print("SoP out= ", out_SoP)
+print("SoP out= ", out_SoP.stokes_vector, "DoP= ",out_SoP.get_DegreeOfPolarization())
 
 sphere = PoincareSphere(parent=view.scene)
 
